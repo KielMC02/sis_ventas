d0b50f3f7ba7570a40e715cb155d2e2175389593
KielMC02 <49813048+KielMC02@users.noreply.github.com>
2020-01-05 10:33:27 -0400

    -Se creo la clase Conexion dentro del proyecto sistema.datos, esta es la clase encargada de realizar la conexion con la base de datos. -Se creo la entidad categoria la cual tiene atributos correspondientes a la tabla categoria de la base de datos. -Se crearon los procedimientos de almacenado correspondientes a las funciones crud de la tabla categoria.

---
+++ Sistema/Sistema.Datos/Conexion.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Datos
+{
+    class Conexion
+    {
+        //Declaramos las variables que almacenaran cada componente de la conexion
+        private string Base;
+        private string Servidor;
+        private string Usuario;
+        private string Clave;
+        private bool Seguridad;
+        private static Conexion Con = null;
+
+        private Conexion()
+        {
+            //Inicializamos todos los Atributos de la Clase
+            this.Base = "dbsistema";
+            this.Servidor = "DKPR8Z1";
+            this.Usuario = "";
+            this.Clave = "";
+            this.Seguridad = true;
+        }
+
+        public SqlConnection CrearConexion()
+        {
+            //Creamos la cadena de conexion
+            SqlConnection Cadena = new SqlConnection();
+            //Utilizamos un Try catch.
+            try
+            {
+                //Establecemos los parametros del String de conexion
+                Cadena.ConnectionString = "Server=" + this.Servidor + "; Database=" + this.Base + ";";
+                //Si utilizamos la seguridad de Windows, le agregamos "Integrated Security = SSPI"
+                if (this.Seguridad)
+                {
+                    Cadena.ConnectionString = Cadena.ConnectionString + "Integrated Security = SSPI";
+                }
+                //Si utilizamos la seguridad de SQL Server le agregamos "User Id=" +  "Password="
+                else
+                {
+                    Cadena.ConnectionString = Cadena.ConnectionString + "User Id=" + this.Usuario+"Password="+this.Clave;
+                }
+            }
+            catch (Exception ex)
+            {
+                //Si hay algun error el contenido de Cadena sera null
+                Cadena = null;
+                throw ex;
+                
+            }
+            //Retornar una variable que sea de tipo Sqlconnection
+            return Cadena;
+        }
+        //Un objeto de la misma clase
+        public static Conexion getInstance()
+        {
+            if (Con == null)
+            {
+                Con = new Conexion();
+
+            }
+            return Con;
+
+        }
+    }
+
+
+}
---
+++ procedimientos_categoria.sql
@@ -0,0 +1,72 @@
+/*PROCEDIMIENTOS DE ALMACENADO PARA LA ENTIDAD CATEGORIA*/
+--Procedimientos Listar
+
+/*Este procedimiento se encarga de listar todas las categorias y las organiza de las mas reciente
+a la mas antigua.*/
+create proc categoria_listar
+as
+select idcategoria as ID,nombre as Nombre,descripcion as Descripcion,estado as Estado
+from categoria
+order by idcategoria desc
+exec categoria_listar
+go
+
+--Procedimientos Buscar
+/*Este Procedimiento se encarga de filtrar las categorias por un estring que inserta el usuario del sistema, el la variable que recibe almacena 
+el string se llama valor y tiene un maximo de 50 caracteres, utilizamos la clausula (Like '%' + '%') para indicar que muestre todos los registro donde
+coincida el el String*/
+create proc categoria_buscar
+@valor varchar(50)
+as
+select idcategoria as ID,nombre as Nombre,descripcion as Descripcion,estado as Estado
+from categoria 
+where nombre like '%' + @valor + '%' or descripcion like '%' + @valor + '%'
+order by nombre desc
+exec categoria_listar
+go
+
+--Procedimientos Insertar
+/*Este procedimiento me permite insertar registros en la tabla categoria, se declaran dos variables las cuales deben ser del mismo tipo y tamano 
+que los campos para los cuales van a ser utilizadas*/
+create proc categoria_insertar
+@nombre varchar(50),
+@descripcion varchar(255)
+as
+insert into categoria (nombre,descripcion) values (@nombre,@descripcion)
+go
+
+--Procedimientos Actualizar
+/*Este procedimiento actualiza los registros de las categorias que correspondan con el ID que el usuairo nos envia desde la aplicacion*/
+create proc categoria_actualizar
+@idcategoria int,
+@nombre varchar(50),
+@descripcion varchar(255)
+as
+update categoria set nombre=@nombre,descripcion=@descripcion
+where idcategoria = @idcategoria
+go
+--Procedimientos Eliminar
+/*Este procedimiento se encarga de eliminar  la categoria que corresponde al ID insertado*/
+create proc categoria_eliminar
+@idcategoria int
+as
+delete from categoria
+where idcategoria=@idcategoria
+go
+
+--Procedimientos Desactivar
+/*Procedimiento de almacenada para desactivar categorias segun el ID que le pasen*/
+create proc categoria_desactivar
+@idcategoria int
+as
+update categoria set estado=0
+where idcategoria = @idcategoria
+go
+--Procedimientos Activar
+/*Este procedimiento activa las categorias segun el id suministrado*/
+create proc categoria_activar
+@idcategoria int
+as
+update categoria set estado=1
+where idcategoria = @idcategoria
+go
