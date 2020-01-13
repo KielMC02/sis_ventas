55a6d0c65f8771cec8e1e9e708086c8b4032ac8b
KielMC02 <49813048+KielMC02@users.noreply.github.com>
2020-01-13 15:57:05 -0400

    -Se creo la Calse Darticulo. -Se Modifico la clase Articulo.

--- Sistema/Sistema.Entidades/Articulo.cs
+++ Sistema/Sistema.Entidades/Articulo.cs
@@ -8,6 +8,7 @@ namespace Sistema.Entidades
         public int Idarticulo { get; set; }
         public int Idcategoria { get; set; }
         public string Codigo { get; set; }
+        public string nombre { get; set; }
         public decimal PrecioVenta { get; set; }
         public int Stock { get; set; }
         public string Descripcion { get; set; }
